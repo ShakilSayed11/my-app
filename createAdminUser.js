09411a1ca8148f2027a19e763856694e9a01f7e3
ShakilSayed11 <147137005+ShakilSayed11@users.noreply.github.com>
2024-08-06 01:23:09 +0530

    Update createAdminUser.js

--- createAdminUser.js
+++ createAdminUser.js
@@ -5,13 +5,14 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient('https://dwcbvbpwkfmydeucsydj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3Y2J2YnB3a2ZteWRldWNzeWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NTQ2NTMsImV4cCI6MjAzODQzMDY1M30.g688zmPnGmwu9oBt7YrfUmtivDohDyiEYPQP-lz16GI');
 
 async function createAdminUser() {
-  const username = 'shakil'; // Change to the desired admin username
-  const password = 'shakil'; // Change to the desired admin password
+  const username = 'admin_username'; // Change to the desired admin username
+  const email = 'admin_email@example.com'; // Change to the admin email
+  const password = 'admin_password'; // Change to the desired admin password
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const { error } = await supabase
     .from('users')
-    .insert([{ username, password: hashedPassword, role: 'admin' }]);
+    .insert([{ username, email, password: hashedPassword, role: 'admin' }]);
 
   if (error) {
     console.error('Error creating admin user:', error);
