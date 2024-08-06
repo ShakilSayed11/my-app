1ba349db654044240f7ec63af896c9aa5ae28db9
ShakilSayed11 <147137005+ShakilSayed11@users.noreply.github.com>
2024-08-06 12:25:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
-
