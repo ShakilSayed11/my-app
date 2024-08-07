cace7b250f6e149f92cd926d07f8545c8cf7211d
ShakilSayed11 <147137005+ShakilSayed11@users.noreply.github.com>
2024-08-07 10:39:46 +0530

    Update productivity-form.js

--- public/productivity-form.js
+++ public/productivity-form.js
@@ -1,7 +1,3 @@
-const { createClient } = window.supabase; // For CDN
-
-const supabase = supabase.createClient('https://dwcbvbpwkfmydeucsydj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3Y2J2YnB3a2ZteWRldWNzeWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NTQ2NTMsImV4cCI6MjAzODQzMDY1M30.g688zmPnGmwu9oBt7YrfUmtivDohDyiEYPQP-lz16GI');
-
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#productivity-form');
 
